4d134275f19d9f9610d84765dea726f1a61bdf0c
P.J.Shand <pete@peteshand.net>
2020-03-16 00:06:40 +1100

    add lime check install

--- install.js
+++ install.js
@@ -1,9 +1,15 @@
 require('./lib/lime-build-lib_xml');
 var haxelibInstall = require('./lib/haxelib-install');
+var limeCheckInstall = require('./lib/lime-check-install');
 var limeSetup = require('./lib/lime-setup');
 
 haxelibInstall().then(() => {
-  limeSetup().then(() => {
-    //console.log("lime setup complete");
+  limeCheckInstall().then(() => {
+    console.log('already installed');
+  }).catch(() => {
+    console.log('not installed');
+    limeSetup().then(() => {
+      console.log("lime setup complete");
+    });
   });
 });
