2038722c5de0203096883e2ea055f9a7148d5d48
P.J.Shand <pete@peteshand.net>
2020-03-16 08:41:37 +1100

    update print messages

--- install.js
+++ install.js
@@ -1,15 +1,16 @@
 require('./lib/lime-build-lib_xml');
+const chalk = require('chalk')
 var haxelibInstall = require('./lib/haxelib-install');
 var limeCheckInstall = require('./lib/lime-check-install');
 var limeSetup = require('./lib/lime-setup');
 
 haxelibInstall().then(() => {
   limeCheckInstall().then(() => {
-    console.log('already installed');
+    console.log(chalk.greenBright('Lime already installed'));
   }).catch(() => {
-    console.log('not installed');
+    console.log(chalk.redBright('Lime not detected'));
     limeSetup().then(() => {
-      console.log("lime setup complete");
+      console.log(chalk.greenBright("Lime setup complete"));
     });
   });
 });
