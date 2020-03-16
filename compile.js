b020ada25d080bf033625b96ee11cd04e1dcb62c
P.J.Shand <pete@peteshand.net>
2020-03-16 14:35:03 +1100

    clean before each build

--- compile.js
+++ compile.js
@@ -14,18 +14,52 @@ if (process.env.PROJECT_XML !== undefined){
 if (process.env.BUILD_TYPE !== undefined){
   buildType = process.env.BUILD_TYPE;
 }
+var defines = [];
+if (process.env.HAXE_DEFINES !== undefined){
+  console.log(process.env.HAXE_DEFINES);
+  console.log(Array.isArray(process.env.HAXE_DEFINES));
+  defines = process.env.HAXE_DEFINES.split(", ").join(",").split(",");
+}
 
-info(buildLime);
+info(() => {
+  clean(() => {
+    buildLime();
+  });
+});
 
-function buildLime() {
-  
+function clean(callback) {
+  var limeProcess = lime("clean", profileXml, "electron");
+  limeProcess.stdout.on('data', (data) => {
+    loglines(data.toString('utf8'), false);
+  });
+  limeProcess.stderr.on('data', (data) => {
+    loglines(data.toString('utf8'), true);
+    
+  });
+  limeProcess.on('close', function (code) {
+    console.log('->',chalk.green('Clear Successfully'));
+    if (callback !== undefined)
+      callback();
+  });
+  limeProcess.on('error', function (err) {
+    console.error('->',chalk.red('Clear Failed'),err);
+    if (callback !== undefined)
+      callback();
+  });
+}
+
+function buildLime(callback) {
+  var args = ["build", profileXml, "electron", buildType];
+  for (var i = 0; i < defines.length; i++){
+    args.push("-D");
+    args.push(defines[i]);
+  }
   console.log(
     '->',
     chalk.blueBright('Haxe Compiler: '),
-    chalk.green("lime", "build", profileXml, "electron", buildType, "-D", "bundled")
+    chalk.green("lime", args.join(" "))
   )
-
-  var limeProcess = lime("build", profileXml, "electron", buildType, "-D", "bundled");
+  var limeProcess = lime(...args);
 
   limeProcess.stdout.on('data', (data) => {
     loglines(data.toString('utf8'), false);
@@ -35,18 +69,13 @@ function buildLime() {
     
   });
   limeProcess.on('close', function (code) {
-    console.log(
-      '->',
-      chalk.blue('Haxe Compiler: '),
-      chalk.green('Built Successfully')
-    )
+    console.log('->',chalk.blue('Haxe Compiler: '),chalk.green('Built Successfully'));
+    if (callback !== undefined)
+      callback();
   });
   limeProcess.on('error', function (err) {
-    console.error(
-      '->',
-      chalk.blue('Haxe Compiler: '),
-      chalk.red('Build Failed'),
-      err
-    )
+    console.error('->',chalk.blue('Haxe Compiler: '),chalk.red('Build Failed'),err);
+    if (callback !== undefined)
+      callback();
   });
 }
